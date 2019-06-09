4a1824e84e7ea8e74e77ff0e6d1000d1450569d8
AlaAmarneh <ala@harri.com>
2019-06-09 21:15:16 +0300

    lines

--- app/src/main/java/com/rccardirector/MapActivity.java
+++ app/src/main/java/com/rccardirector/MapActivity.java
@@ -1,6 +1,7 @@
 package com.rccardirector;
 
 import android.annotation.SuppressLint;
+import android.graphics.Point;
 import android.graphics.PointF;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -54,8 +55,71 @@ public class MapActivity extends AppCompatActivity {
     }
 
     public void click(View view) {
-        carDirection = App.Direction.SOUTH;
-        String path = imgMap.getSelectedPath();
-        Log.d(TAG, "path: " + path);
+//        carDirection = App.Direction.SOUTH;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 0;
+        int y2 = 0;
+
+        for (int i = 0; i < imgMap.getSelectedPathPoints().size(); i++) {
+            Point point = imgMap.getSelectedPathPoints().get(i);
+            if (i == 0) {
+                x1 = point.x;
+                y1 = point.y;
+                continue;
+            }
+            if ((point.x != x1 && point.y != y1 || i == imgMap.getSelectedPathPoints().size() - 1)) {
+                Log.d(TAG, "new Line" + x1 + " " + y1 + " " + x2 + " " + y2);
+
+                x1 = x2;
+                y1 = y2;
+            } else {
+                x2 = point.x;
+                y2 = point.y;
+            }
+        }
+    }
+
+    class Line {
+        private float x1, x2, y1, y2;
+
+        public Line(float x1, float x2, float y1, float y2) {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+        }
+
+        public float getX1() {
+            return x1;
+        }
+
+        public void setX1(float x1) {
+            this.x1 = x1;
+        }
+
+        public float getX2() {
+            return x2;
+        }
+
+        public void setX2(float x2) {
+            this.x2 = x2;
+        }
+
+        public float getY1() {
+            return y1;
+        }
+
+        public void setY1(float y1) {
+            this.y1 = y1;
+        }
+
+        public float getY2() {
+            return y2;
+        }
+
+        public void setY2(float y2) {
+            this.y2 = y2;
+        }
     }
 }
